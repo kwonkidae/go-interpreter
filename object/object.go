6d7b5ef2323713c7da649714b92f2d87065cbaa2
kwon <kkdosk@naver.com>
2020-07-27 09:07:23 +0900

    error describe

--- object/object.go
+++ object/object.go
@@ -5,9 +5,11 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
 
 type Object interface {
@@ -24,6 +26,22 @@ type Boolean struct {
 }
 
 type Null struct{}
+
+type ReturnValue struct {
+	Value Object
+}
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+
+func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 
