81ab48ae25b7ff3793da4f6ba7dca55425ae38c5
kwonkidae <kkdosk@naver.com>
2020-08-23 19:27:34 +0900

    call

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -60,6 +60,15 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		env.Set(node.Name.Value, val)
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
 	}
 
 	return nil
